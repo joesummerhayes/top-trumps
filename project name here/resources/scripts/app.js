738529e1c9fa58bde3a7f0f11b1b2d48127cad39
Joe <joesummerhayes09@hotmail.com>
2018-11-23 18:18:19 +0000

    player stats for you mate

--- project name here/resources/scripts/app.js
+++ project name here/resources/scripts/app.js
@@ -2,7 +2,16 @@ $(document).foundation()
 
 $(document).ready(function(){
 
-    var playerStatsExample = [88, 79, 54, 96,25,73];
+    var statsTorres = [40, 65, 80, 40, 35, 70];
+    var statsGerrard = [25, 90, 90, 70, 95, 55];
+    var statsLampard = [55, 87, 90, 75, 90, 50];
+    var statsBullard = [90, 40, 60, 60, 50, 75];
+    var statsToure = [65, 70, 80, 72, 60, 30];
+    var statsLuiz = [80, 40, 50, 80, 40, 25];
+    var statsHenry = [65, 70, 95, 82, 80, 85];
+    var statsOwen = [20, 45, 85, 65, 50, 75];
+    var statsMilner = [85, 78, 75, 78, 87, 20];
+
     console.log(playerStatsExample);
 
 
