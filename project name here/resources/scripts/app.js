9a01d89253645559890cfa97eb444d0eeda0f749
Oliver Smith <oliver17.smith@gmail.com>
2018-12-09 21:25:20 +0000

    Cards styling, Dual card creation, html structuring

--- project name here/resources/scripts/app.js
+++ project name here/resources/scripts/app.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
 // function that picks a new card
 
-var newCard = function() {
+var newCardRight = function() {
     var no = Math.floor(Math.random() * 10);
 
     if (no === 1) {
@@ -45,27 +45,80 @@ var newCard = function() {
 
     //display a player's stats on the card
 
-    document.querySelector('.cp-stat').textContent = player[0];
-    document.querySelector('.l-stat').textContent = player[1];
-    document.querySelector('.bgi-stat').textContent = player[2];
-    document.querySelector('.wfa-stat').textContent = player[3];
-    document.querySelector('.cl-stat').textContent = player[4];
-    document.querySelector('.sa-stat').textContent = player[5];
-    document.querySelector('.player-name').textContent = player[6];
+    document.querySelector('.trump-card-right .cp-stat').textContent = player[0];
+    document.querySelector('.trump-card-right .l-stat').textContent = player[1];
+    document.querySelector('.trump-card-right .bgi-stat').textContent = player[2];
+    document.querySelector('.trump-card-right .wfa-stat').textContent = player[3];
+    document.querySelector('.trump-card-right .cl-stat').textContent = player[4];
+    document.querySelector('.trump-card-right .sa-stat').textContent = player[5];
+    document.querySelector('.trump-card-right .player-name').textContent = player[6];
 
-    ilonaKep();
+    ilonaKepRight();
+
+}
+var newCardLeft = function() {
+    var no = Math.floor(Math.random() * 10);
+
+    if (no === 1) {
+        player = statsTorres
+    } else if (no === 2) {
+        player = statsGerrard
+    } else if (no === 3) {
+        player = statsLampard
+    } else if (no === 4) {
+        player = statsBullard
+    } else if (no === 5) {
+        player = statsToure
+    } else if (no === 6) {
+        player = statsLuiz
+    } else if (no === 7) {
+        player = statsHenry
+    } else if (no === 8) {
+        player = statsOwen
+    } else {
+        player = statsMilner
+    };
+
+    console.log(player);
+
+    //display a player's stats on the card
+
+    document.querySelector('.trump-card-left .cp-stat').textContent = player[0];
+    document.querySelector('.trump-card-left .l-stat').textContent = player[1];
+    document.querySelector('.trump-card-left .bgi-stat').textContent = player[2];
+    document.querySelector('.trump-card-left .wfa-stat').textContent = player[3];
+    document.querySelector('.trump-card-left .cl-stat').textContent = player[4];
+    document.querySelector('.trump-card-left .sa-stat').textContent = player[5];
+    document.querySelector('.trump-card-left .player-name').textContent = player[6];
+
+    ilonaKepLeft();
 
 }
 
-document.querySelector('.new-card').addEventListener('click', newCard);
+document.querySelector('.new-card').addEventListener('click', newCardLeft);
+document.querySelector('.new-card').addEventListener('click', newCardRight);
 
-function ilonaKep() {
-    var frame = document.querySelector('.frame');
-    var ilona = document.createElement("IMG");
-    ilona.src = player[7];
-    frame.appendChild(ilona);
+function ilonaKepRight() {
+    // var frame = document.querySelector('.frame');
+    // var ilona = document.createElement("IMG");
+    // ilona.src = player[7];
+    // frame.appendChild(ilona);
+    document.getElementById("trump-image-right").src = player[7];
+  }
+
+  
+
+
+
+function ilonaKepLeft() {
+    // var frame = document.querySelector('.frame');
+    // var ilona = document.createElement("IMG");
+    // ilona.src = player[7];
+    // frame.appendChild(ilona);
+    document.getElementById("trump-image-left").src = player[7];
   }
 
   
 
 });
+
